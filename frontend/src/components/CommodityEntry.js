d909a9335cbad3778bb705126d00085a008e9f3d
macniel <christoph.baehr@live.com>
2021-12-11 12:05:19 +0100

    fix safari background image bug

--- frontend/src/components/CommodityEntry.js
+++ frontend/src/components/CommodityEntry.js
@@ -3,7 +3,17 @@ import NumberInput from "./NumberInput";
 import ShipSelector from "./ShipSelector";
 import "./CommodityEntry.css";
 import classNames from "classnames";
-import ShopSelector from "./ShopSeelctor";
+
+import { ReactComponent as OreIcon } from "../assets/metal.svg";
+import { ReactComponent as GasIcon } from "../assets/foam.svg";
+import { ReactComponent as GemsIcon } from "../assets/gems.svg";
+import { ReactComponent as DrugsIcon } from "../assets/medicines.svg";
+import { ReactComponent as MedicalIcon } from "../assets/hospital-cross.svg";
+import { ReactComponent as FoodIcon } from "../assets/knife-fork.svg";
+import { ReactComponent as AgriculturalIcon } from "../assets/wheat.svg";
+import { ReactComponent as NaturalIcon } from "../assets/three-leaves.svg";
+import { ReactComponent as WasteIcon } from "../assets/waste.svg";
+import { ReactComponent as RecycleIcon } from "../assets/recycle.svg";
 
 function CommodityEntry() {
   const [quantity, setQuantity] = useState("");
@@ -43,6 +53,36 @@ function CommodityEntry() {
     });
   };
 
+  const renderSvg = (kind) => {
+    switch (kind) {
+      case "metal":
+        return <OreIcon />;
+      case "gas":
+      case "halogen":
+        return <GasIcon />;
+      case "mineral":
+        return <GemsIcon />;
+      case "drug":
+      case "vice":
+        return <DrugsIcon />;
+      case "medical":
+        return <MedicalIcon />;
+      case "food":
+        return <FoodIcon />;
+      case "agricultural":
+        return <AgriculturalIcon />;
+      case "natural":
+        return <NaturalIcon />;
+      case "waste":
+        return <WasteIcon />;
+      case "junk":
+      case "scrap":
+      case "temporary":
+      default:
+        return <RecycleIcon />;
+    }
+  };
+
   return (
     <div className="spatial-layout">
       <div className="rows">
@@ -52,15 +92,12 @@ function CommodityEntry() {
             {commodities?.map((commodityItem) => (
               <button
                 onClick={() => setCommodity(commodityItem)}
-                className={classNames(
-                  "commodity",
-                  "commodity--" + commodityItem.kind.toLowerCase(),
-                  {
-                    active: commodity.code === commodityItem.code,
-                  }
-                )}
+                className={classNames("commodity", {
+                  active: commodity.code === commodityItem.code,
+                })}
                 title={commodityItem.name}
               >
+                {renderSvg(commodityItem.kind.toLowerCase())}
                 <span className="commodity__label">{commodityItem.code}</span>
               </button>
             ))}
